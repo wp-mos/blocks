b302a8a20f64450c6f7e48247568fed8310a3dd5
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-01-01 21:13:10 +0000

    fix: modal close button

--- build/blocks/auth-modal/index.asset.php
+++ build/blocks/auth-modal/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd03ffcde09731c8e6f1f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2937567c25eef098775a');
--- includes/blocks/user.php
+++ includes/blocks/user.php
@@ -11,17 +11,10 @@ function mos_user_render_cb($attributes) {
 
   ?>
 
-  <div><?php echo esc_html( $current_user->user_login) ?></div>
-  <div>User email: <?php echo esc_html( $current_user->user_email  )  ?></div>
+  <h2><?php echo esc_html( $current_user->user_login) ?></h2>
+  <h6>Email: <?php echo esc_html( $current_user->user_email  )  ?></h6>
 
   <?php
-
-  // printf( __( 'Username: %s', 'textdomain' ), esc_html( $current_user->user_login ) ) . '<br />';
-  // printf( __( 'User email: %s', 'textdomain' ), esc_html( $current_user->user_email ) ) . '<br />';
-  // printf( __( 'User first name: %s', 'textdomain' ), esc_html( $current_user->user_firstname ) ) . '<br />';
-  // printf( __( 'User last name: %s', 'textdomain' ), esc_html( $current_user->user_lastname ) ) . '<br />';
-  // printf( __( 'User display name: %s', 'textdomain' ), esc_html( $current_user->display_name ) ) . '<br />';
-  // printf( __( 'User ID: %s', 'textdomain' ), esc_html( $current_user->ID ) );
 
   $output = ob_get_contents();
   ob_end_clean();
