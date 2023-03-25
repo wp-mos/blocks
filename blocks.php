1dca38de726e86f8e1dccbf1f08d3a0e26e419de
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-03-25 19:02:50 +0000

    bkp: last commit

--- blocks.php
+++ blocks.php
@@ -1,47 +1,47 @@
 <?php
 
-/**
- * Plugin Name:       Mos Blocks
- * Theme URI: https: //github.com/wp-mos/blocks
- * Author: Harpoon Team
- * Author URI: https://harpoon.ro/
- * Description:       A plugin for custom adding blocks to Mos Theme.
- * Version:           1.0.0
- * Requires at least: 5.9
- * Requires PHP:      7.2
- * Text Domain:       /wp-mos/blocks
- * Domain Path:       /languages
- */
+  /**
+   * Plugin Name:       Mos Blocks
+   * Theme URI: https: //github.com/wp-mos/blocks
+   * Author: Harpoon Team
+   * Author URI: https://harpoon.ro/
+   * Description:       A plugin for custom adding blocks to Mos Theme.
+   * Version:           0.0.1
+   * Requires at least: 6.0
+   * Requires PHP:      7.2
+   * Text Domain:       /wp-mos/blocks
+   * Domain Path:       /languages
+   */
 
-if (!function_exists('add_action')) {
-	echo 'Seems like you stumbled here by accident. 😛';
-	exit;
-}
+  if (!function_exists('add_action')) {
+    echo 'Seems like you stumbled here by accident. 😛';
+    exit;
+  }
 
 // Setup
-define('MOS_PLUGIN_DIR', plugin_dir_path(__FILE__));
+  define('MOS_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
 // Includes
-$rootFiles         = glob(MOS_PLUGIN_DIR . 'includes/*.php');
-$subdirectoryFiles = glob(MOS_PLUGIN_DIR . 'includes/**/*.php');
-$allFiles          = array_merge($rootFiles, $subdirectoryFiles);
+  $rootFiles = glob(MOS_PLUGIN_DIR . 'includes/*.php');
+  $subdirectoryFiles = glob(MOS_PLUGIN_DIR . 'includes/**/*.php');
+  $allFiles = array_merge($rootFiles, $subdirectoryFiles);
 
-foreach ($allFiles as $filename) {
-	include_once($filename);
-}
+  foreach ($allFiles as $filename) {
+    include_once($filename);
+  }
 
 // Filters
-add_filter('block_categories_all', 'mos_register_category');
-add_filter('upload_mimes', 'allow_dfx_files_mime');
+  add_filter('block_categories_all', 'mos_register_category');
+  add_filter('upload_mimes', 'allow_dfx_files_mime');
 
 // Hooks
-add_action('rest_api_init', 'mos_rest_api_init');
-add_action('init', 'mos_register_blocks');
+  add_action('rest_api_init', 'mos_rest_api_init');
+  add_action('init', 'mos_register_blocks');
 
-add_action('wp_enqueue_scripts', 'mos_enqueue_scripts');
-add_action('admin_init', 'mos_redirect_subscribers_to_frontend');
-add_action('wp_loaded', 'mos_hide_admin_bar');
+  add_action('wp_enqueue_scripts', 'mos_enqueue_scripts');
+  add_action('admin_init', 'mos_redirect_subscribers_to_frontend');
+  add_action('wp_loaded', 'mos_hide_admin_bar');
 
-add_action('template_redirect', 'mos_redirect_if_not_logged_in');
-add_action('template_redirect', 'mos_redirect_if_logged_in');
-add_action('template_redirect', 'mos_redirects');
+  add_action('template_redirect', 'mos_redirect_if_not_logged_in');
+  add_action('template_redirect', 'mos_redirect_if_logged_in');
+  add_action('template_redirect', 'mos_redirects');
---
+++ includes/blocks/auth-button.php
@@ -0,0 +1,46 @@
+<?php
+
+  function mos_auth_button()
+  {
+    $user = wp_get_current_user();
+    $user_logged_in = $user->exists();
+
+    $name = $user_logged_in ? $user->user_login : __('Sign in', 'mos');
+
+    ob_start();
+
+    ?>
+      <div class="wp-block-mos-blocks-auth-link has-dropdown">
+
+        <?php
+          if (!$user_logged_in) {
+            ?>
+              <a class="mos-blocks-sign-in-link" href="<?php echo esc_url(site_url('/autentificare')) ?>">
+                <?php echo $name; ?>
+              </a>
+          <?php } ?>
+
+        <?php
+          if ($user_logged_in) { ?>
+              <a class="mos-blocks-sign-in-link" href="#">
+                <?php echo $name; ?>
+              </a>
+              <div class="dropdown">
+                  <div class="dropdown-wrapper">
+                      <a href="<?php echo esc_url(site_url('/utilizatori/contul-meu')) ?>"
+                         class="dropdown-item">Contul Meu</a>
+                      <a href="<?php echo esc_url(site_url('/utilizatori/comanda-noua')) ?>"
+                         class="dropdown-item">Comanda Noua</a>
+                      <a href="<?php echo wp_logout_url(home_url()) ?>" class="dropdown-item">Logout</a>
+                  </div>
+              </div>
+          <?php } ?>
+
+      </div>
+    <?php
+
+    $output = ob_get_contents();
+    ob_end_clean();
+
+    return $output;
+  }
